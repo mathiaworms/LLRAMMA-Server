41fab427ad9e97697032a6d356602cfe6d0e269e
mathiaworms <mathias.123@hotmail.fr>
2024-06-16 21:54:05 +0200

    add static for do some test with llm gpt2 before integrating some api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'), // Chemin absolu vers le répertoire des fichiers statiques
+      serveRoot: '/static', // URL de base pour les fichiers statiques
+    }),
+    // Autres modules de votre application
+  ],
+  // Autres configurations et exports de votre module
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import * as path from 'path';
+import * as cors from 'cors'; // Importer cors depuis le module cors
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.use('/gpt2', express.static(path.join(__dirname, '..', 'public', 'gpt2')));
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
